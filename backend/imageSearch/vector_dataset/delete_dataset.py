02f46067a02d08c63f9208cb62a21cebf028f3c7
thesourcedapp <thesourcedapp@gmail.com>
2025-12-23 16:18:50 -0800

    Update delete_dataset.py

--- backend/imageSearch/vector_dataset/delete_dataset.py
+++ backend/imageSearch/vector_dataset/delete_dataset.py
@@ -2,13 +2,6 @@ from qdrant_client import QdrantClient
 from qdrant_client.models import PointsSelector
 
 
-#Authorization
-QDRANT_URL = "https://77cba74c-2354-4dad-91fa-dfd55f032cbb.us-east4-0.gcp.cloud.qdrant.io"
-QDRANT_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.HkUrGA1QsrF5_bH3j8iFtzSxLXhdj7GTfoiFy8WPVvU"
-
-#name of database
-COLLECTION_NAME = "clothing_database"
-
 
 client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY)
 print("Connected to Qdrant cloud")
